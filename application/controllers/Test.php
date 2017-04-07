631422cb7d3b251e2023a71ca8f9a8ba05b8b53b
junming4 <2284876299@qq.com>
2017-04-07 17:12:10 +0800

    ci测试

---
+++ application/controllers/Test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/7
+ * Time: 16:50
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends CI_Controller
+{
+    public function index(){
+        echo "test";
+    }
+}
