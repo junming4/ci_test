7717c8d050d175f6093e16a8e3249098d7ef2d45
junming4 <2284876299@qq.com>
2017-04-07 17:21:07 +0800

    edit

---
+++ application/controllers/admin/Test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/7
+ * Time: 17:19
+ */
+
+class Test extends CI_Controller {
+
+    public function index(){
+        echo "admin_test";
+    }
+}
